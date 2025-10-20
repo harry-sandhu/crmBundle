ca2e298d0cee8513b2401ec0c0b418e3c6cb94ce
harry-sandhu <120314298+harry-sandhu@users.noreply.github.com>
2025-10-20 15:53:56 +0530

    unexpercted fix

--- backend/src/routes/adminEarningsRoutes.ts
+++ backend/src/routes/adminEarningsRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { repairOldEarnings } from "../controllers/adminEarningsRepairController";
+
 import { getUserFullDetails, getUsersEarningsSummary } from "../controllers/adminEarningController";
 
 const router = express.Router();
 
 // Only for admins or one-time fix (you can disable after running once)
-router.post("/repair", repairOldEarnings);
+
 router.get("/users-summary", getUsersEarningsSummary);
 router.get("/user/:refCode/details", getUserFullDetails);
 
